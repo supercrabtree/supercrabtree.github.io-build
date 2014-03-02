7472b94ba09b279a2439b6c44eab20796d88963a
George Crabtree <supercrabtree@googlemail.com>
2014-03-02 16:58:04 +1100

    add initial files from previous build

---
+++ app/scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('supercrabtreegithubioBuildApp', [
+  'ngRoute'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main',
+        controller: 'MainCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'partials/contact',
+        controller: 'ContactCtrl'
+      })
+      .when('/projects', {
+        templateUrl: 'partials/projects',
+        controller: 'ProjectsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
